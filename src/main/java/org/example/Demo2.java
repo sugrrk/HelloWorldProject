969b1960c5fb28be5d8f5099a346cd05b73a9cb9
Ramesh <sugrrk06@gmail.com>
2021-05-10 23:03:29 -0700

    This is hello world project

---
+++ src/main/java/org/example/Demo2.java
@@ -0,0 +1,10 @@
+package org.example;
+
+public class Demo2 {
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 15;
+        int c = a+b;
+        System.out.println(c);
+    }
+}
---
+++ src/test/java/org/example/Demo1.java
@@ -0,0 +1,7 @@
+package org.example;
+
+public class Demo1 {
+    public static void main(String[] args) {
+        System.out.println("This is HelloWorld Project");
+    }
+}
